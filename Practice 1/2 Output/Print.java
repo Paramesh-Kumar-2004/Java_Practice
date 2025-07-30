d1263df561a13f9ae4f63c133ecef950aecf1233
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-07-30 19:49:26 +0530

    Writing Output Code

---
+++ Practice 1/2 Output/Print.java
@@ -0,0 +1,13 @@
+import java.lang.System; // For Print
+
+
+class Print{
+    public static void main (String args[])
+    {
+        int VP = 2004;
+        String PV = "VP";
+        System.out.println(VP);
+        System.out.println(PV);
+    }
+}
+
