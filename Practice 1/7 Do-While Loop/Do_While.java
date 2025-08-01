80629ad70b434618ef1a9a81e016459465480bef
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-01 19:46:57 +0530

    Do_While Practice

---
+++ Practice 1/7 Do-While Loop/Do_While.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Do_While{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+
+        int num = 0;
+        do{
+            num+=1;
+            System.out.println(num+" : VP");
+        }
+        while(num<5);
+
+
+        // Get int from user grater than 10, get int until user enter greater than 10
+        System.out.println("\nGet int from user grater than 10, get int until user enter greater than 10");
+        int num2 = 0;
+        do{
+            System.out.print("Enter The Number : ");
+            num2 = input.nextInt();
+        }while(num2 <=10);
+
+    }
+}
