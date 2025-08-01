f0cb11707e778a5e0ebfb7f88ec43db10143fc2b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-01 19:51:51 +0530

    Update PrintArray.java

--- Practice 1/4 Array/PrintArray.java
+++ Practice 1/4 Array/PrintArray.java
@@ -2,10 +2,13 @@ import java.util.Arrays;
 
 class PrintArray{
     public static void main(String args[]){
+
         String[] arr = new String[2];
+
         arr[0] = "VP";
         arr[1] = "PV";
 
         System.out.println(Arrays.toString(arr));
     }
+    
 }
