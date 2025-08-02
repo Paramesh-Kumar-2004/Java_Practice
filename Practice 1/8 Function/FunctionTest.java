e3023cc7649d0e092373f7f5da5cb39c64f634fe
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-02 19:21:50 +0530

    Function Practice

---
+++ Practice 1/8 Function/FunctionTest.java
@@ -0,0 +1,22 @@
+
+class FunctionTest{
+
+    void add(int V, int P){
+        System.out.println("V : "+V+"\nP : "+P+"\nV + P : "+(V+P));
+        Test();
+    }
+
+    void Test(){
+        System.out.println("\nFunction Call From Add Function, Without Creating An Object\n Only We Can Call The Function Static to Static");
+    }
+
+    static void StaticFunctionTest(){
+        System.out.println("\nStatic Function \nWe Can Call The Static To Static, Without Creating An Object");
+    }
+
+    public static void main(String[] args){
+        FunctionTest obj1 = new FunctionTest();
+        obj1.add(4,8);
+        StaticFunctionTest();
+    }
+}
