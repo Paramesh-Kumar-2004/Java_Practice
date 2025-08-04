8b8e2741eb7dfe13d87e97f09986da611c57ae13
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-04 18:06:20 +0530

    Practice This_Keyword

---
+++ Practice 1/13 This Keyword/ThisKeyword.java
@@ -0,0 +1,29 @@
+
+class ThisKeyword {
+
+    String UserName = "";
+
+    void SetNameWithoutThis(String UserName) {
+        System.out.println("Set Name Without This Keyword");
+        UserName = UserName;  // UserName it take from function's parameters.
+        System.out.println("User Name Without This : " + UserName);
+    }
+
+    void SetNameWithThis(String UserName) {
+        System.out.println("Set Name With This Keyword");
+        this.UserName = UserName; // This Keyword this.UserName Means - obj1.UserName, Obj2.UserName
+        System.out.println("User Name With This : " + UserName);
+    }
+
+    public static void main(String[] args) {
+
+        ThisKeyword obj1 = new ThisKeyword();
+
+        obj1.SetNameWithoutThis("Name");
+        System.out.println(obj1.UserName);
+
+        ThisKeyword obj2 = new ThisKeyword();
+        obj2.SetNameWithThis("VP");
+
+    }
+}
