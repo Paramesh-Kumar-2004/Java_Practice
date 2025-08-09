1c0924a1adbffcde497e349586a16d16c2b270c0
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-09 19:02:02 +0530

    Write A Code For Exception_Handling

---
+++ Practice 1/21 Exception Handling/Exception_Handling.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Exception Handling Is Handle Run Time Errors
+// try,catch
+
+
+class Exception_Handling{
+    public static void main(String [] args){
+        System.out.println("Exception Handler");
+
+        Scanner input = new Scanner(System.in);
+        
+        try{
+            System.out.print("Enter The Number VP : ");
+            int VP = input.nextInt();
+            System.out.println("VP : "+VP);
+        }
+
+        catch(Exception e){ // General Exception Handler For All Exception Errors.
+            System.out.println("\nError : \n"+e+"\n");
+        }
+        
+    }
+}
