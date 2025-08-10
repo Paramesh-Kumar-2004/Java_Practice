b5c21f3eed59b2f9a4322956cf1928ace6b58b12
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-10 18:25:12 +0530

    Practice Finally Keyword

---
+++ Practice 1/22 Finally Keyword/FinallyKeyword.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// try,catch,finally
+
+
+class FinallyKeyword{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Finally");
+        
+        int VP = 0;
+
+        try{
+            VP = input.nextInt();
+        }
+        catch(Exception e){
+            System.out.println("Error : "+e);
+        }
+        finally{
+            System.out.println("Finally Result Is : "+VP);
+        }
+    }
+}
