bab7b75b9421454ca685828a0d27ef4466b3ad5f
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-11 18:56:13 +0530

    Practice ThrowsKeyword

---
+++ Practice 1/25 Throws Keyword/ThrowsKeyword.java
@@ -0,0 +1,24 @@
+
+// Who Are All using this class they should handle the errors created by the calling class(here Division).
+// If I Use throws Exception - Called : Checked Exception Error.
+// If I Not Use throws Exception - Called : UnChecked Exception Error.
+
+
+class Division{
+    void Div() throws Exception{
+        int VP = 10/0;
+    }
+}
+
+class ThrowsKeyword{
+    public static void main(String[] args){
+        System.out.println("Throws Keyword");
+        Division obj1 = new Division();
+        try{
+            obj1.Div();
+        }
+        catch(Exception e){
+            System.out.println("Error : "+e);
+        }
+    }
+}
