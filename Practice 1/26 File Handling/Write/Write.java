2c450a7fc880d8cbedc636f00bb213126b028170
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-12 17:41:30 +0530

    Test code for Write In File - 2

--- Practice 1/26 File Handling/Write/Write.java
+++ Practice 1/26 File Handling/Write/Write.java
@@ -9,16 +9,16 @@ class Write{
     public static void main(String[] args){
         
         try{
-            // For Normal Text File
-            FileWriter fw = new FileWriter("Output.txt",true);
-            fw.write("VP\n\n");
-            fw.write("The Text Was Written By Java, FileWriter...");
-            fw.append(" Testing Append\n\n"); // Append Means in Object we should means true
+            // // For Normal Text File
+            // FileWriter fw = new FileWriter("Output.txt",true);
+            // fw.write("VP\n\n");
+            // fw.write("The Text Was Written By Java, FileWriter...");
+            // fw.append(" Testing Append\n\n"); // Append Means in Object we should means true
             
-            // // For Python File
-            // FileWriter fw = new FileWriter("Print.py");
-            // fw.write("vp = 'VP'\n\n");
-            // fw.write("print(vp)");
+            // For Python File
+            FileWriter fw = new FileWriter("Print.py");
+            fw.write("vp = 'VP'\n\n");
+            fw.write("print(vp)");
 
             fw.close();
             System.out.println("Success");
