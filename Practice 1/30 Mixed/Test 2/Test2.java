e65a6cd3c9902c688142b33109b645f93092fab5
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-14 18:58:50 +0530

    Practice

---
+++ Practice 1/30 Mixed/Test 2/Test2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Test2{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("VP");
+
+        System.out.print("Enter The int : ");
+        int VP = input.nextInt();
+        input.nextLine();
+        System.out.print("Enter The Str : ");
+        String PV = input.nextLine();
+        System.out.print("Enter The Chr : ");
+        char VV = input.next().charAt(0);
+
+        System.out.println("\nInt - VP : "+VP);
+        System.out.println("Str - PV : "+PV);
+        System.out.println("Chr - VP : "+VV);
+    }
+}
