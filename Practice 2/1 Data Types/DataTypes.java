f5b05ad14dd894b383f2a6e4f78f81558a95c7de
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-15 18:34:25 +0530

    Practice 2 : DataTypes

---
+++ Practice 2/1 Data Types/DataTypes.java
@@ -0,0 +1,24 @@
+
+class DataTypes{
+    public static void main(String[] args){
+        byte Byte = 84;
+        short Short = 2004;
+        int Interger = 2004;
+        long Long = 1_4_2004_8_3_2004l; // Instead of Commas We Can Use "_"
+
+        float Float = 2004.84f;
+        double Double = 0.1_4__8_3__2004;
+
+        String STRING = "VP";
+        char Char = 'V';
+
+        System.out.println("Byte : "+Byte);
+        System.out.println("Short : "+Short);
+        System.out.println("Integer : "+Interger);
+        System.out.println("Long : "+Long);
+        System.out.println("Float : "+Float);
+        System.out.println("Double : "+Double);
+        System.out.println("String : "+STRING);
+        System.out.println("Char : "+Char);
+    }
+}
