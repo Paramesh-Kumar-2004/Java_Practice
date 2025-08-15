b20580a3a928f8fcc72621781982b1923ce1051d
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-15 18:40:27 +0530

    Practice Input_Output

---
+++ Practice 2/2 Input _ Output/Input_Output.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class Input_Output{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Eneter The Number  : ");
+        int VP = input.nextInt();
+        input.nextLine();
+        System.out.print("Eneter The String  : ");
+        String PV = input.nextLine();
+        System.out.print("Enter The Char : ");
+        char VV = input.next().charAt(0);
+
+        // input.nextLong(), input.nextFloat(), input.nextDouble()
+
+        System.out.println("Number : "+VP);
+        System.out.println("String : "+PV);
+        System.out.println("Char   : "+VV);
+    }
+}
