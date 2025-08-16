6b689d93e8265fd2230156db1477bff920e76484
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-16 18:22:10 +0530

    Practice Escape_Sequence

---
+++ Practice 2/4 Escape Sequences & Command Lines/Escape_Sequence.java
@@ -0,0 +1,16 @@
+// Single Command Line
+/* Multi
+Command 
+Line
+*/
+
+class Escape_Sequence{
+    public static void main(String[] args){
+        System.out.println("Hello \"VP\" , I Love You!\n");
+
+        System.out.println("\\t\t Tap Space");
+
+        // \n , \b , \" , \n etc...
+
+    }
+}
