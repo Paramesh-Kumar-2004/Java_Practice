bf970dcd94de4bda5cdaf2c8aa560b93a557f5e1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-16 18:20:23 +0530

    Practice TypeCasting

---
+++ Practice 2/3 Type Casting/TypeCasting.java
@@ -0,0 +1,14 @@
+
+class TypeCasting{
+    public static void main(String[] args){
+
+        int VP = 65;
+        System.out.println("The Number Is "+VP+" And The Char (Type Casting) : "+(char)VP);
+        
+        System.out.print("Type Casting : ");
+        for(int i=65; i<=90; i++){
+            System.out.print((char)i +" ");
+        }
+
+    }
+}
