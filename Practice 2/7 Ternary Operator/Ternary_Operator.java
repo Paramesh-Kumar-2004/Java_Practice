866bf4448b64c70f626b5fb4d5f18142e85b642b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-17 18:55:07 +0530

    Practice Ternary_Operator

---
+++ Practice 2/7 Ternary Operator/Ternary_Operator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Ternary_Operator{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ternary Operator");
+
+        System.out.print("Enter The Number : ");
+        int VP = scan.nextInt();
+
+        String PV = (VP<0) ? "Negative Number" : "Positive Number";
+        System.out.println("Ternary With Three Condition : "+PV);
+
+
+        String VV = (VP<0) ? "Negative Number" : (VP>0) ? "Positive Number" : "Neutral Number";
+        System.out.print("Ternary With Two Condition : "+VV);
+    }
+}
