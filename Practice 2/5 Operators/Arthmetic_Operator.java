7638dec6301864e42907aed0c9143dcb6de78d63
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-17 19:20:00 +0530

    Update

--- Practice 2/5 Operators/Arthmetic_Operator.java
+++ Practice 2/5 Operators/Arthmetic_Operator.java
@@ -1,7 +1,6 @@
 
-
-class Arthmetic_Operator{
-    public static void main(String[] args){
+class Arthmetic_Operator {
+    public static void main(String[] args) {
 
         System.out.println("Arthmetic Operators\n");
 
@@ -9,84 +8,75 @@ class Arthmetic_Operator{
         int VP = 4;
         int PV = 8;
 
-        System.out.println("Values : \nVP : "+VP+"\nPV : "+PV+"\n");
-        System.out.println("Add : "+(VP+PV));
-        System.out.println("Min : "+(VP-PV));
-        System.out.println("Mul : "+(VP*PV));
-        System.out.println("Div : "+(VP/PV));
-        System.out.println("Mod : "+(VP%PV));
+        System.out.println("Values : \nVP : " + VP + "\nPV : " + PV + "\n");
+        System.out.println("Add : " + (VP + PV));
+        System.out.println("Min : " + (VP - PV));
+        System.out.println("Mul : " + (VP * PV));
+        System.out.println("Div : " + (VP / PV));
+        System.out.println("Mod : " + (VP % PV));
 
-        System.out.println("\nDiv With Float : "+(float)(VP/PV));
-
-
+        System.out.println("\nDiv With Float : " + (float) (VP / PV));
 
         System.out.println("\n\nCompound Assignment\n");
-        VP+=4;
-        System.out.println("VP+=4 : "+VP);
-        PV-=4;
-        System.out.println("PV-=4 : "+PV);
+        VP += 4;
+        System.out.println("VP+=4 : " + VP);
+        PV -= 4;
+        System.out.println("PV-=4 : " + PV);
         // PV+=4 , PV-=4 , PV*=4 , PV%=4 , PV/=4
-        
- 
 
         System.out.println("\n\nIncrement/Decrement\n");
         int a = 1;
         ++a; // 2 // Pre Increment
         a++; // 3 // Post Increment
-        System.out.println("++a & a++ : "+a); // 3
+        System.out.println("++a & a++ : " + a); // 3
 
         int b = 1;
         --b; // 0
         b--; // -1
-        System.out.println("--a & a-- : "+b); // -1
-
-
+        System.out.println("--a & a-- : " + b); // -1
 
         System.out.println("\n\nRelational Operators\n");
         // < , <= , > , >= , == , !=
-        System.out.println("VP : "+VP+"\tPV : "+PV);
-        System.out.println("<  : "+(VP<PV));
-        System.out.println("<= : "+(VP<=PV));
-        System.out.println(">  : "+(VP>PV));
-        System.out.println(">= : "+(VP>=PV));
-        System.out.println("== : "+(VP==PV));
-        System.out.println("!= : "+(VP!=PV));
-
-
+        System.out.println("VP : " + VP + "\tPV : " + PV);
+        System.out.println("<  : " + (VP < PV));
+        System.out.println("<= : " + (VP <= PV));
+        System.out.println(">  : " + (VP > PV));
+        System.out.println(">= : " + (VP >= PV));
+        System.out.println("== : " + (VP == PV));
+        System.out.println("!= : " + (VP != PV));
 
         System.out.println("\n\nBitwise Operators\n");
-        // AND , OR , NOT , EXOR 
+        // AND , OR , NOT , EXOR
         // << left shift, >> right shift , >>> right shift zero fill
         int a2 = 4;
         int b2 = 8;
-        System.out.println("a&b : "+(a2&b2));
-        System.out.println("a|b : "+(a2|b2));
-        System.out.println("~a , ~b : "+(~a2)+" , "+~b2);
+        System.out.println("a&b : " + (a2 & b2));
+        System.out.println("a|b : " + (a2 | b2));
+        System.out.println("~a , ~b : " + (~a2) + " , " + ~b2);
 
-        // Right Shift : Multipling the int with 2. 
+        // Right Shift : Multipling the int with 2.
         /*
-        Ex : a = 4 (0000 0100) (Position a[0] Will Be Removed)
-        a<<1 : 0000 1000
-        a<<2 : 0001 0000
-        */
-        System.out.println("\nLeft Shift :\n"+(a2<<1));
-        System.out.println(a2<<2);
-        System.out.println(a2<<3);
+         * Ex : a = 4 (0000 0100) (Position a[0] Will Be Removed)
+         * a<<1 : 0000 1000
+         * a<<2 : 0001 0000
+         */
+        System.out.println("\nLeft Shift :\n" + (a2 << 1));
+        System.out.println(a2 << 2);
+        System.out.println(a2 << 3);
 
         // Left Shift : .
         int a3 = 4;
-        System.out.println("\nRight Shift :\n"+(a3>>1));
-        System.out.println(a3>>2);
-        System.out.println(a3>>3);
-
-
+        System.out.println("\nRight Shift :\n" + (a3 >> 1));
+        System.out.println(a3 >> 2);
+        System.out.println(a3 >> 3);
 
         System.out.println("\nShort Circuit Logical Operators\n");
-        // && , || 
+
+        // && , ||
         int a4 = 4;
-        int b4 = 8;
-        System.out.println("&& : "+((a4<=4) && (a4<=8)));
-        System.out.println("|| : "+((a4<=4) || (a4<8)));
+        // int b4 = 8;
+        System.out.println("&& : " + ((a4 <= 4) && (a4 <= 8)));
+        System.out.println("|| : " + ((a4 <= 4) || (a4 < 8)));
 
     }
 }
--- Practice 2/6 Condition Statement/If_Condition.java
+++ Practice 2/6 Condition Statement/If_Condition.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 
-class If_Condition{
-    public static void main(String[] args){
+class If_Condition {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter The Number : ");
         int VP = scan.nextInt();
-        if(VP>0){
-            System.out.print(VP+" Is Positive");
-            if(VP%2==0){
+        if (VP > 0) {
+            System.out.print(VP + " Is Positive");
+            if (VP % 2 == 0) {
                 System.out.print(" And Even");
-            }
-            else{
+            } else {
                 System.out.print(" And Odd");
             }
-        }
-        else if(VP<0){
-            System.out.print(VP+" Is Negative");
-            if(VP%2==0){
+        } else if (VP < 0) {
+            System.out.print(VP + " Is Negative");
+            if (VP % 2 == 0) {
                 System.out.print(" And Even");
-            }
-            else{
+            } else {
                 System.out.print(" And Odd");
             }
+        } else {
+            System.out.print(VP + " Is Neutral");
         }
-        else{
-            System.out.print(VP+" Is Neutral");
-        }
+
+        scan.close();
     }
 }
