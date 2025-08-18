9cdc5946aa79d1a03613e981f6eb19a24e4d7025
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-18 18:46:45 +0530

    Practice For Loops

---
+++ Practice 2/10 Loops/For/For_Loop.java
@@ -0,0 +1,14 @@
+
+
+class For_Loop{
+    public static void main(String[] args){
+
+        System.out.println("For Loop");
+
+        int VP = 14;
+
+        for(int i=0; i<=VP; i++){
+            System.out.print(i+":* ");
+        }
+    }
+}
