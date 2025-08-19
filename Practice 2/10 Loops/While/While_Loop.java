0817f1c074037cbc4ba3cf44a7048f88534d655b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-19 18:57:38 +0530

    While Loop Practice

---
+++ Practice 2/10 Loops/While/While_Loop.java
@@ -0,0 +1,14 @@
+
+
+class For_Loop{
+    public static void main(String[] args){
+        System.out.println("While Loop");
+
+        int PV = 1;
+
+        while(PV<=14){
+            System.out.print(PV+":* ");
+            PV+=1;
+        }
+    }
+}
