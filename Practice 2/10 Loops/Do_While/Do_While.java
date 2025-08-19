07dd82dbd84674b789b95cd24b617c9678b5335c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-19 19:02:03 +0530

    Practice Do_While Loop

---
+++ Practice 2/10 Loops/Do_While/Do_While.java
@@ -0,0 +1,14 @@
+
+
+class Do_While{
+    public static void main(String[] args){
+        System.out.println("Do While Loop");
+        int PV = 1;
+
+        do{
+            System.out.print(PV+":* ");
+            PV+=2004;
+        }
+        while(PV<=14);
+    }
+}
